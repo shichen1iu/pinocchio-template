e05609cd652346472cb728ee78769b0386577b36
tonton <19677766+tonton-sol@users.noreply.github.com>
2025-03-15 01:29:58 +0900

    Shank error support for Pinocchio (#3)
    * add this error
    
    * add thiserror derive macros
    
    * add shank with alpha for pinocchio error support
    
    * update idle with errors

--- program/src/error.rs
+++ program/src/error.rs
@@ -1,8 +1,9 @@
 use pinocchio::program_error::ProgramError;
 
 /// Errors that may be returned by myproject.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
 pub enum MyProjectError {
+    #[error("Invalid")]
     Invalid,
 }
 
